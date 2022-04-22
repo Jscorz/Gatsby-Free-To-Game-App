3133e233b96d7a0bca5f47e902b0d46ed7d3aaf4
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-22 12:55:43 -0600

    added css file and added footer/navbar to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,8 +2,17 @@ import React from "react"
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 
-const Layout = () => {
-  return <div>Layout</div>
+import "normalize.css"
+import "../assets/css/styles.css"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  )
 }
 
 export default Layout
