861ede21481df0c739b7c0e735a53b71ef5bd20e
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-23 13:25:25 -0600

    added basic footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,23 @@
 import React from "react"
+import styled from "styled-components"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <Wrapper>
+      <footer className="page-footer">
+        <p>
+          &copy; {new Date().getFullYear()}
+          <span> Free Gamer</span>. All rights reserved
+        </p>
+      </footer>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.section`
+  p {
+    letter-spacing: 0.25rem;
+  }
+`
 
 export default Footer
