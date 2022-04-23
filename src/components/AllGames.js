84f364aa59188f31503d65ef8215f21bf402194c
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-23 13:50:19 -0600

    edited to display only 10 games while developing a bit

--- src/components/AllGames.js
+++ src/components/AllGames.js
@@ -1,24 +1,37 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import styled from "styled-components"
 
 const AllGames = () => {
   const {
     example: { data },
   } = useStaticQuery(query)
+
+  const shortData = data.slice(0, 10)
   console.log(data)
 
   return (
-    <section className="page">
-      {data.map(game => {
-        return (
-          <section key={game.id}>
-            <h1>{game.title}</h1>
-          </section>
-        )
-      })}
-    </section>
+    <Wrapper>
+      <section className="page">
+        {shortData.map(game => {
+          return (
+            <section key={game.id}>
+              <h1>{game.title}</h1>
+              <h2>{game.publisher}</h2>
+            </section>
+          )
+        })}
+      </section>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.section`
+  section {
+    border: 1px solid white;
+    padding: 2rem;
+  }
+`
 
 export const query = graphql`
   {
