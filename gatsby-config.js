d6209ecbad11931dbd3decdef7fb54cabbd91cce
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-24 09:12:18 -0600

    added hero and installed gatsby transformer sharp

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,12 +9,22 @@ const { buildSchema, buildClientSchema } = require("graphql")
 module.exports = {
   /* Your site config here */
   plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-graphql`,
       options: {
         typeName: `FreetoGame`,
         fieldName: `FreetoGame`,
         url: `https://www.freetogame.com/api/games`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
+import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/Layout"
 
 export default function Home() {
   return (
     <Layout>
-      <main>Free To Game Application</main>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/hero.jpg"
+            alt="controller for gaming"
+            className="hero-img"
+            placeholder="tracedSVG"
+            layout="fullWidth"
+          />
+          <article className="hero-container">
+            <div className="hero-text">
+              <h1>Free Gamer</h1>
+              <h4>No More Wasted Money</h4>
+            </div>
+          </article>
+        </header>
+      </main>
     </Layout>
   )
 }
