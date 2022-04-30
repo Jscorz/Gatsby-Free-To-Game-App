e1ac938a26bf058776496e5d1f7f5c008cfe9b96
Justin Scorzafava <jkscorzafava@gmail.com>
2022-04-30 08:21:08 -0600

    removed commented out previous solution

--- gatsby-node.js
+++ gatsby-node.js
@@ -25,36 +25,4 @@ exports.sourceNodes = async ({
       },
     })
   })
-  //   const games = await Promise.all(
-  //     results.map(async result => {
-  //       const { url } = result
-  //       const gameResponse = await fetch(url)
-  //       return await gameResponse.json()
-  //     })
-  //   )
 }
-
-// const fetch = (...args) =>
-//   import(`node-fetch`).then(({ default: fetch }) => fetch(...args))
-// exports.sourceNodes = async ({
-//   actions: { createNode },
-//   createContentDigest,
-// }) => {
-//   // get data from GitHub API at build time
-//   const result = await fetch(`https://www.freetogame.com/api/games`)
-//   const resultData = await result.json()
-//   // create node for build time data example in the docs
-//   createNode({
-//     // nameWithOwner and url are arbitrary fields from the data
-//     data: resultData,
-//     url: resultData.html_url,
-//     // required fields
-//     id: `example-build-time-data`,
-//     parent: null,
-//     children: [],
-//     internal: {
-//       type: `example`,
-//       contentDigest: createContentDigest(resultData),
-//     },
-//   })
-// }
