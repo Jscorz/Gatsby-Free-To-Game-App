e059f8efddbc870b281f9e7c0a9f99a30152ee4e
Justin Scorzafava <jkscorzafava@gmail.com>
2022-05-26 15:20:39 -0600

    added animations to hero img and text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,98 @@
-import { StaticImage } from "gatsby-plugin-image"
-import React from "react"
+import React, { useRef, useLayoutEffect } from "react"
 import { Link } from "gatsby"
 import Layout from "../components/Layout"
 import ShortListGames from "../components/ShortListGames"
 import SEO from "../components/SEO"
+import { gsap } from "gsap"
+import heroImg from "../assets/images/hero.jpg"
+// import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
+  const el = useRef()
+  const q = gsap.utils.selector(el)
+  const tl = useRef()
+
+  useLayoutEffect(() => {
+    tl.current = gsap
+      .timeline()
+
+      .fromTo(
+        q(".hero-img"),
+        0.75,
+        {
+          opacity: 0,
+          scale: 3.5,
+          easeInOut: "power2",
+        },
+
+        { opacity: 1, scale: 1 },
+        "+=0.25"
+      )
+      .fromTo(
+        q("#firstWord"),
+        0.7,
+        {
+          y: -1000,
+          opacity: 0,
+          ease: "elastic",
+        },
+        {
+          y: 0,
+          opacity: 1,
+        }
+      )
+      .fromTo(
+        q("#secondWord"),
+        0.7,
+        {
+          y: 1000,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          opacity: 1,
+        },
+        "-=.7"
+      )
+      .fromTo(
+        q("h4"),
+        0.7,
+        {
+          y: 100,
+          opacity: 0,
+          scale: 2,
+          ease: "back",
+        },
+        {
+          y: 0,
+          scale: 1,
+          opacity: 1,
+        }
+      )
+  }, [])
+
   return (
     <Layout>
       <SEO title="Home" />
       <main className="page">
-        <header className="hero">
-          <StaticImage
+        <header className="hero" ref={el}>
+          <img src={heroImg} alt="pc gaming" className="hero-img" />
+          {/* <StaticImage
             src="../assets/images/hero.jpg"
             alt="pc gaming"
             className="hero-img"
-            placeholder="tracedSVG"
+            // placeholder="tracedSVG"
+            Style={`
+              overflow: hidden;
+            `}
             layout="fullWidth"
-          />
+          /> */}
           <article className="hero-container">
             <div className="hero-text">
-              <h1>Free Gamer</h1>
+              <h1>
+                <span id="firstWord">Free </span>
+                <span id="secondWord">Gamer</span>
+              </h1>
               <h4>No More Wasted Money</h4>
             </div>
           </article>
